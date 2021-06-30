603f4f10bec21e6543572ef41b1369f93c5a087d
talyaakov <tal.yaakov@sap.com>
2021-06-30 19:55:12 +0300

    finalize the mock server with the test config and registry.

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.tools.sap/sm-fake-callback-client
 
 go 1.16
+
+require (
+	github.com/google/uuid v1.2.0
+	github.com/tidwall/gjson v1.8.0
+)
--- main.go
+++ main.go
@@ -1,53 +1,144 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/google/uuid"
+	"github.com/tidwall/gjson"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"sync"
+	//"sync"
 )
 
+var (
+	tests map[string]testSettings
+	mutex *sync.Mutex
+)
+
+type testSettings struct {
+	ID                    string      `json:"id"`
+	ProvisionStatus       int64       `json:"provisionStatus"`
+	ProvisionResponseBody interface{} `json:"provisionResponseBody"`
+	DeprovisionStatus     int64       `json:"deprovisionStatus"`
+}
+
 func main() {
+	tests = make(map[string]testSettings)
+	mutex = &sync.Mutex{}
 	log.Printf("starting mock server")
+
+	/*
+		POST:
+		 Create a new test configuration, using testSettings struct
+		 Response will contain the ID of the test config
+		DELETE:
+		 Optional flags:
+			* all=true: delete all test configurations
+			* id=uuid: delete single test configuration
+	*/
+	http.HandleFunc("/setup", setup)
+
+	/*
+		Mandatory flag:
+		* id=testUUID: with define which test config to work with
+		POST:
+		 Execute create with the test config
+		DELETE:
+		 Execute delete with the test config
+	*/
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fail := r.URL.Query().Get("fail")
-
 	switch r.Method {
 	case "GET":
 		fmt.Fprintf(w, "Fake Callback Application")
 	case "POST":
-		handleProvision(w, fail)
+		handleProvision(w, r)
 	case "DELETE":
-		handleDeprovision(w, fail)
+		handleDeprovision(w, r)
 	}
 	return
 }
+func setup(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		getTest(w, r)
+		return
+	case "POST":
+		createTest(w, r)
+		return
+	case "DELETE":
+		deleteTest(w, r)
+		return
+	}
+}
 
-func handleProvision(w http.ResponseWriter, fail string) {
-	if fail == "true" {
-		w = generateResponse(w, 400, nil)
+func createTest(w http.ResponseWriter, r *http.Request) {
+	testUUID := uuid.New().String()
+
+	// Read body
+	b, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
 		return
 	}
 
-	body := []byte("{\"accessUrl\": \"accessUrl.com\",\"metadata\": {\"key1\": \"value1\",\"key2\": \"value2\"}}")
-	w = generateResponse(w, 201, body)
-}
-
-func handleDeprovision(w http.ResponseWriter, fail string) {
-	if fail == "true" {
-		w = generateResponse(w, 410, nil)
+	// Unmarshal
+	var reqTestSettings testSettings
+	err = json.Unmarshal(b, &reqTestSettings)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
 		return
 	}
 
-	w = generateResponse(w, 200, nil)
+	mutex.Lock()
+	settings := testSettings{
+		ID:                    testUUID,
+		ProvisionStatus:       gjson.GetBytes(b, "provisionStatus").Int(),
+		ProvisionResponseBody: gjson.GetBytes(b, "provisionResponseBody").Value(),
+		DeprovisionStatus:     gjson.GetBytes(b, "deprovisionStatus").Int(),
+	}
+	tests[testUUID] = settings
+	mutex.Unlock()
+	log.Printf("Creating a test with ID: %s", testUUID)
+	generateResponse(w, 201, tests[testUUID])
 }
 
-func generateResponse(w http.ResponseWriter, statusCode int, body []byte) http.ResponseWriter {
+func getTest(w http.ResponseWriter, r *http.Request) {
+	testUUID := r.URL.Query().Get("id")
+	generateResponse(w, 200, tests[testUUID])
+}
+
+func deleteTest(w http.ResponseWriter, r *http.Request) {
+	deleteAll := r.URL.Query().Get("all")
+	if deleteAll == "true" {
+		tests = make(map[string]testSettings)
+	} else {
+		testUUID := r.URL.Query().Get("id")
+		delete(tests, testUUID)
+	}
+	generateResponse(w, 200, nil)
+}
+
+func handleProvision(w http.ResponseWriter, r *http.Request) {
+	testUUID := r.URL.Query().Get("id")
+	w = generateResponse(w, int(tests[testUUID].ProvisionStatus), tests[testUUID].ProvisionResponseBody)
+}
+
+func handleDeprovision(w http.ResponseWriter, r *http.Request) {
+	testUUID := r.URL.Query().Get("id")
+	w = generateResponse(w, int(tests[testUUID].DeprovisionStatus), nil)
+}
+
+func generateResponse(w http.ResponseWriter, statusCode int, object interface{}) http.ResponseWriter {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
-	w.Write(body)
+	marshal, _ := json.Marshal(object)
+	w.Write(marshal)
 	return w
 }
